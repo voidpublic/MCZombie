485a8e967a123d5169df87b9c58de73c5c2bb625
voidpublic <sebioeller@hotmail.com>
2014-12-28 12:04:29 +0100

    Custom /eat messages

--- Commands/CmdEat.cs
+++ Commands/CmdEat.cs
@@ -17,6 +17,7 @@ namespace MCForge.Commands
         public CmdEat() { }
         public override void Use(Player p, string message)
         {
+            if (p == null){  Player.SendMessage(p, c.red + "Sorry consoles can't eat cookies ;(");  return; }
             if (p.money > 0)
             {
                 if (p.stuffed)
@@ -31,39 +32,38 @@ namespace MCForge.Commands
                     {
                         p.stuffed = false;
                     };
-                    Random nr = new Random();
-                    int msgnr = nr.Next(0, 20);
-                    string msg = "";
-                    switch (msgnr)
-                    {
-                        case 0: msg = "ate a cookie"; break;
-                        case 1: msg = "nommed a cookie"; break;
-                        case 2: msg = "got eaten by a cookie"; break;
-                        case 3: msg = "couldnt get enough from one cookie"; break;
-                        case 4: msg = "was aroused by a cookie"; break;
-                        case 5: msg = "bloodthirst increased, because a cookie was eaten"; break;
-                        case 6: msg = "has killed a cookie"; break;
-                        case 7: msg = "ate them all"; break;
-                        case 8: msg = "tasted bacon cookies"; break;
-                        case 9: msg = "accidently ate a supercookie"; break;
-                        case 10: msg = "got high by eating special cookies"; break;
-                        case 11: msg = "ate Fido's dog biscuit!"; break;
-                        case 12: msg = "is on a diet, No cookie was eaten!"; break;
-                        case 13: msg = "'s cookie was sugar free :("; break;
-                        case 14: msg = "typed /eat"; break;
-                        case 15: msg = "was slain by the cookie army "; break;
-                        case 16: msg = "thought that brains were overrated and ate a cookie instead"; break;
-                        case 17: msg = "joyfully ate a cookie in the middle of a zombie apocalypse"; break;
-                        case 18: msg = "just ate a Scooby Snack!"; break;
-                        case 19: msg = "had a rainbow cookie"; break;
-                        case 20: msg = "tried to eat the cookie monster"; break;
-                    }
                     p.money -= 1;
-                    Player.GlobalMessage(p.color + p.name + Server.DefaultColor + " " + msg);
+                    Player.GlobalMessage(GetEatMessage(p));
                 }
             }
             else
                 Player.SendMessage(p,"You have not got any cookies to eat");
+        }
+
+        public string GetEatMessage(Player eater)
+        {
+            string path = "text/eatmessages.txt";
+            if (!File.Exists(path))
+            {
+                string[] prepared = new string[] 
+                { "#Use <name> to display eat messages", 
+                  "<name> ate a cookie",
+                  "<name> nommed a cookie",
+                  "<name> got eaten by a cookie", 
+                  "<name> ate them all"
+                };
+                File.WriteAllLines(path, prepared);
+            }
+            string[] lines = File.ReadAllLines(path);
+            List<string> messages = new List<string>();
+            foreach (string msg in lines)
+            {
+                if (!msg.StartsWith("#")) messages.Add(msg);
+            }
+            int useline = new Random().Next(0, messages.Count());
+            string toreturn = messages[useline];
+            toreturn = toreturn.Replace("<name>", eater.group.color + eater.name + Server.DefaultColor);
+            return toreturn;
         }
         public override void Help(Player p)
         {
